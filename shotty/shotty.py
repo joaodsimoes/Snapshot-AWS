a97b3dbe54d85933228cabbfca41b10467a0fcba
Joao <joao@Joaos-MacBook-Pro.local>
2020-02-13 23:55:43 +0100

    List ec2 instances now tabulated using prettytable

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,8 +1,22 @@
 import	boto3
+import click
+from prettytable import PrettyTable
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+t = PrettyTable(['id', 'Instance type', 'AvailabilityZone', 'state','public_dns_name'])
+
+def list_instances():
+	for i in ec2.instances.all():
+		t.add_row([
+		i.id,
+		i.instance_type,
+		i.placement['AvailabilityZone'],
+		i.state['Name'],
+		i.public_dns_name])
+	print(t)	
+
+
 
 if __name__ == '__main__':
-	session = boto3.Session(profile_name='shotty')
-	ec2 = session.resource('ec2')
-	
-	for i in ec2.instances.all():
-		print(i)
+	list_instances()
